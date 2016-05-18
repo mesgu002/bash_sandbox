dfe5f7893ff6ba6782f7a2bd999b1554b0b62af7
Montana Esguerra <mesgu002@ucr.edu>
2016-05-18 10:49:03 -0700

    revised largest.sh

--- largest.sh
+++ largest.sh
@@ -4,8 +4,16 @@ first=$1
 second=$2
 third=$3
 
-if [ $first -gt $second ]; then
-	echo "$first is bigger"
+if [ "$first" -gt "$second" ] && [ "$first" -gt "$third" ]; then
+	echo "$first is the largest of the three"
 else
-	echo "$second is bigger"
+	if [ "$second" -gt "$first" ] && [ "$second" -gt "$third" ]; then
+		echo "$second is the largest of the three"
+	else
+		if [ "$third" -gt "$first" ] && [ "$third" -gt "$second" ]; then
+			echo "$third is the largest of the three"
+		else
+			echo "All three numbers are equal"
+		fi
+	fi
 fi
